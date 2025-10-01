02f185ef7a37b18147876882db361a997ac5e1a8
james <brayanamengle9@gmail.com>
2025-10-01 02:26:23 +0100

    plan

--- database/migrations/2025_09_30_224328_create_plans_table.php
+++ database/migrations/2025_09_30_224328_create_plans_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('plans', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->decimal('monthly_price', 10, 2)->default(0);
+            $table->decimal('yearly_price', 10, 2)->default(0);
+            $table->json('perks')->nullable();
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('plans');
